aaba5789711bd179197f2c8581f2a56b6939c245
kyleceguera <163008271+kyleceguera@users.noreply.github.com>
2024-07-01 11:02:25 -0700

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ today = datetime.today().date()
 # tour_names.insert(0, '')
 
 # Streamlit app
-st.set_page_config(page_title='Input Relevant Information', page_icon='dollar', layout="centered", initial_sidebar_state="auto", menu_items=None)
+st.set_page_config(page_title='TTC Cancellation Tool', page_icon='dollar', layout="centered", initial_sidebar_state="auto", menu_items=None)
 
 
 def calc_refund(amount_paid, claim_amount):
